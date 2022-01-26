a389317fa21a3a97ca76de79ffe93488c906ef37
regan hayes <hayesregan@yahoo.com>
2022-01-25 23:25:43 -0500

    added POST

--- src/App.js
+++ src/App.js
@@ -10,9 +10,7 @@ import {
 import BestBooks from './BestBooks';
 import Profile from './Profile';
 import Login from './Login';
-import { Form , Button, Container } from 'react-bootstrap';
 
-import axios from 'axios';
 
 class App extends React.Component {
 
@@ -35,20 +33,8 @@ class App extends React.Component {
     })
   }
 
-  handleBookSubmit = (e) => {
-    e.preventDefault();
-    let newBook = {
-      title: e.target.title.value,
-      description: e.target.description.value,
-      status: e.target.status.value,
-      email: e.target.email.value
-    }
-    console.log(newBook);
-  }
 
-  makeBook = async (newBook) => {
-    let bookResult = await axios.post();
-  }
+
 
   render() {
     return (
@@ -57,41 +43,12 @@ class App extends React.Component {
           <Header user={this.state.user} onLogout={this.logoutHandler} />
           <Switch>
             <Route exact path="/">
-              {this.state.user ? <BestBooks /> : <Login onLogin={this.loginHandler}/>}
+              {this.state.user ? <BestBooks /> : <Login onLogin={this.loginHandler} />}
             </Route>
             <Route exactPath="/profile">
-              <Profile user={this.state.user}/>
+              <Profile user={this.state.user} />
             </Route>
           </Switch>
-          <Container>
-            <Form onSubmit={this.handleBookSubmit}>
-              <Form.Group controlId = "title"> 
-                <Form.Label>
-                  Title
-                </Form.Label>
-                <Form.Control type="text" />
-              </Form.Group>
-              <Form.Group controlId = "description"> 
-                <Form.Label>
-                  Description
-                </Form.Label>
-                <Form.Control type="text" />
-              </Form.Group>
-              <Form.Group controlId = "status"> 
-                <Form.Label>
-                  Status
-                </Form.Label>
-                <Form.Control type="text" />
-              </Form.Group>
-              <Form.Group controlId = "email"> 
-                <Form.Label>
-                  Email
-                </Form.Label>
-                <Form.Control type="text" />
-              </Form.Group>
-              <Button type="submit">Add a Book</Button>
-            </Form>
-          </Container>
           <Footer />
         </Router>
       </>
